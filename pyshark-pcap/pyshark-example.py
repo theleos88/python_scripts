d4244c2ef9fab7f5936fce447a34b8ab75fee0a1
Leonardo Linguaglossa <theleos88@gmail.com>
2017-02-13 18:14:16 +0100

    Pyshark parsing example

---
+++ pyshark-pcap/pyshark-example.py
@@ -0,0 +1,46 @@
+import pyshark
+import signal
+import os
+import string
+
+counter=0
+ipcounter=0
+tcpcounter=0
+udpcounter=0
+
+filename='/home/leos/Dropbox/work/vpp_fdio/VPP_SVN/datasets/smia2012central.cap'
+
+cap = pyshark.FileCapture(filename)
+
+i=0
+
+for packet in cap:
+
+    if 'ip' not in packet:
+        print "OTHER"
+        print packet.eth.src, packet.eth.dst.int_value
+    else:
+        print "Ip packet here"
+
+    #for a in packet:
+    #    print a
+
+    if packet.transport_layer == 'TCP':
+
+
+        ip = None
+        #ip_version = get_ip_version(packet)
+        #if ip_version == 4:
+        ip = packet.ip
+        #elif ip_version == 6:
+        #    ip = packet.ipv6
+        print 'Packet %d' % i
+        print 'Source IP        -', ip.src.int_value
+        print 'Source port      -', packet.tcp.srcport
+        print 'Destination IP   -', ip.dst
+        print 'Destination port -', packet.tcp.dstport
+        print
+    i += 1
+
+    if i> 10:
+        break
