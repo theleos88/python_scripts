5e35f489144c6fd5357ee341dba70ba21c9c1653
Leonardo Linguaglossa <theleos88@gmail.com>
2017-08-20 20:27:27 +0200

    Makefile rules: clean and test

--- simple_duplicates/README.md
+++ simple_duplicates/README.md
@@ -1,5 +1,30 @@
 # Organizing my data
 
+## Dependencies
+
+- parallel
+
+## Test Dir
+
+```
+├── one
+│   └── file1
+├── three
+│   ├── file1
+│   └── file2
+└── two
+    └── file1
+```
+
+In particular:
+
+one/file1 | two/file1 = Same data, same name
+one/file1 | three/file1 = Different data, same name
+one/file1 | three/file2 = Same data, Different name
+
+Of course, this also imply a case with different data and different name.
+
+
 ``` 
 	find DIR -type f -printf "%h %f %s\n" | python main.py -t d [--dest DESTDIR]
 ```
--- simple_duplicates/main.py
+++ simple_duplicates/main.py
@@ -2,11 +2,58 @@ import sys
 import pickle
 import argparse
 import os
+import csv
+
+
+def export_csv(ds, filename):
+
+    writer = csv.writer(open(filename, 'wb'))
+
+    for key, value in ds.iteritems():
+        ln = [key]
+        for ik, iv in value.iteritems():
+            ln.append(ik)
+            ln.append(iv)
+            # ln.extend([v for v in iv])
+
+        writer.writerow(ln)
+
+
+def save_obj(ds, args):
+    with open(args.dest, "w") as outfile:
+        pickle.dump(ds, outfile, -1)
+
+
+def load_obj(args):
+    ds = {}
+    with open(args.show) as infile:
+        ds = pickle.load(infile)
+    return ds
 
 
 def parse_files(ds, args):
     for fn in sys.stdin:
-        print (fn)
+        info = fn.strip().split(" ")
+        print (info[0], info[1], info[2])
+
+        # Check name + size
+        name = info[1]+info[2]
+
+        # Creating if not existing
+        if name not in ds.keys():
+            ds[name] = {}
+            ds[name]["dup"] = 0
+        else:
+            ds[name]["dup"] += 1
+
+            # Since it is a dup, we should add a new name
+            name = name+"_"+str(ds[name]["dup"])
+            ds[name] = {}
+
+        ds[name]["type"] = "f"
+        ds[name]["size"] = info[2]
+        ds[name]["parent"] = info[0]
+    save_obj(ds, args)
 
 
 def parse_directory(ds, args):
@@ -18,11 +65,18 @@ if (__name__ == '__main__'):
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--type", help="Parsing files (f) or directories (d)", type=str, required=True)
     parser.add_argument("-d", "--dest", help="Destination for the object type", type=str)
+    parser.add_argument("-c", "--csv", help="Destination for the csv file", type=str)
+    parser.add_argument("-s", "--show", help="Simply display the data structure", type=str)
 
     args = parser.parse_args()
 
     print("Type: ", args.type)
     print("Dest: ", args.dest)
+    print("CSV: ", args.csv)
+
+    if (args.show is not None and os.path.exists(args.show)):
+        print ("Opening", args.show)
+        print (load_obj(args))
 
     # Read the data structure (if it exists)
     if (args.dest is not None):
@@ -41,3 +95,6 @@ if (__name__ == '__main__'):
         parse_directory(ds, args)
     if (args.type == "f"):
         parse_files(ds, args)
+
+    if (args.csv is not None):
+        export_csv(ds, args.csv)
